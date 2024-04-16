20ef2af913307bacc70a33ccf27ced2c5c9d571a
“maulanaFandi” <maulana27fandi@gmail.com>
2024-04-16 08:31:52 +0700

    Fix Carrousel

--- client/cms/src/components/Pricing.jsx
+++ client/cms/src/components/Pricing.jsx
@@ -1,22 +1,25 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import Chip from "@mui/material/Chip";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
 import Container from "@mui/material/Container";
-import Divider from "@mui/material/Divider";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import AutoAwesomeIcon from "@mui/icons-material/AutoAwesome";
-import CheckCircleRoundedIcon from "@mui/icons-material/CheckCircleRounded";
 import Carousel from "react-material-ui-carousel";
 import { Card } from "@mui/material";
 import { useState } from "react";
 import { useEffect } from "react";
 import axios from "axios";
-import CardMedia from "@mui/material/CardMedia";
 import { Link } from "react-router-dom";
+import ButtonBase from "@mui/material/ButtonBase";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+
+const Img = styled("img")({
+  margin: "auto",
+  display: "block",
+  maxWidth: "100%",
+  maxHeight: "100%",
+});
 
 export default function Pricing() {
   const [data, setData] = useState([]);
@@ -89,44 +92,55 @@ export default function Pricing() {
         }}
         sx={{
           width: "100%",
-          maxWidth: "300px", // Max width to prevent too much stretching
+          maxWidth: 500, // Max width to prevent too much stretching
           marginX: "auto", // Center the carousel
         }}>
         {data.map((result) => (
-          <Grid key={result._id} item xs={12} sm={result.title} md={4}>
-            <Card
-              sx={{
-                maxWidth: 345,
-                alignItems: "center",
-                justifyContent: "center",
-                display: "flex",
-                flexDirection: "column",
-              }}>
-              <CardMedia
-                component="img"
-                alt="imageUrl"
-                maxheight="150"
-                maxwidth="150"
-                image={result.imageUrl}
-              />
-              <CardContent sx={{ textAlign: "center" }}>
-                <Typography gutterBottom variant="h5" component="div">
-                  {result.name}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {result.description}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {formatPrice(result.price)}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Link to={`/login`}>
-                  <Button size="small">View Detail</Button>
-                </Link>
-              </CardActions>
-            </Card>
-          </Grid>
+          <Paper
+            key={result._id}
+            sx={{
+              p: 2,
+              margin: "auto",
+              maxWidth: 500,
+              flexGrow: 1,
+              backgroundColor: (theme) =>
+                theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+            }}>
+            <Grid container spacing={2}>
+              <Grid item>
+                <ButtonBase sx={{ width: 128, height: 128 }}>
+                  <Img alt="imageUrl" src={result.imageUrl} />
+                </ButtonBase>
+              </Grid>
+              <Grid item xs={12} sm container>
+                <Grid item xs container direction="column" spacing={2}>
+                  <Grid item xs>
+                    <Typography
+                      gutterBottom
+                      variant="subtitle1"
+                      component="div">
+                      {result.name}
+                    </Typography>
+                    <Typography variant="body2" gutterBottom>
+                      {result.description}
+                    </Typography>
+                  </Grid>
+                  <Grid item>
+                    <Link to={`/login`}>
+                      <Button variant="contained" size="small" color="primary">
+                        View Detail
+                      </Button>
+                    </Link>
+                  </Grid>
+                </Grid>
+                <Grid item>
+                  <Typography variant="subtitle1" component="div">
+                    {formatPrice(result.price)}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Paper>
         ))}
       </Carousel>
     </Container>
